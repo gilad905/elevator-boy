7b12f375a4f7372ba2db59c742993125e2b5ff22
Gilad Mayani <gilad905@gmail.com>
2024-12-20 18:24:27 +0100

    godot empty project

--- public/modules/elevator.mjs
+++
@@ -1,7 +0,0 @@
-import * as constants from "./constants.mjs";
-import * as canvas from "./canvas.mjs";
-
-export function draw() {
-  const { measures, colors, defaultLineWidth } = constants;
-  canvas.drawRect(measures.building, false, defaultLineWidth, colors.elevator);
-}
--- server.mjs
+++
@@ -1,5 +0,0 @@
-import express from "express";
-const app = express();
-app.use(express.static("public"));
-app.listen(80);
-console.log(`listening on http://localhost`);
