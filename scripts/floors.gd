778f70037aa5c6b9e4c2eb809fa1a250ccee6ba7
Gilad Mayani <gilad905@gmail.com>
2024-12-23 00:07:17 +0100

    elevator enter intervals; TimeScale

--- scripts/floors.gd
+++ scripts/floors.gd
@@ -2,7 +2,3 @@ extends Node2D
 
 func get_floor(floor_num: int) -> Node:
 	return get_node("Floor_" + str(floor_num))
-
-
-func _on_elevator_enter_timer_timeout() -> void:
-	pass # Replace with function body.
--- scripts/main.gd
+++ scripts/main.gd
@@ -14,13 +14,29 @@ func _process(_delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
+	elif event.is_action_pressed("time_scale_increase"):
+		set_time_scale(true)
+	elif event.is_action_pressed("time_scale_decrease"):
+		set_time_scale(false)
 
 func _on_persons_timer_timeout() -> void:
 	$Persons.add_random_person()
 
 func _on_door_state_changed(state: int) -> void:
-	if state == $Door.State.open:
+	if state == $Elevator/Door.State.open:
 		$Elevator.remove_persons_in_dest()
 		$ElevatorEnterTimer.start()
-	elif state == $Door.State.closing:
+	elif state == $Elevator/Door.State.closing:
 		$ElevatorEnterTimer.stop()
+
+func _on_elevator_enter_timer_timeout():
+	var floor_num = $Elevator.current_floor_num
+	var _floor = $Floors.get_floor(floor_num)
+	_floor.enter_elevator_next()
+
+func set_time_scale(to_increase: bool):
+	var time_scale = Engine.get_time_scale()
+	var shift = 2.0 if to_increase else 0.5
+	time_scale = clamp(time_scale * shift, 0.125, 16.0)
+	Engine.set_time_scale(time_scale)
+	$TimeScale.text = str(time_scale)
