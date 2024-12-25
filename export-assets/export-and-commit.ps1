a25469abc3f10206b43a003acf012562e5da1542
Gilad Mayani <gilad905@gmail.com>
2024-12-25 13:44:31 +0100

    asset restructures

--- export-assets/export-and-commit.ps1
+++ export-assets/export-and-commit.ps1
@@ -1,10 +1,8 @@
-cd exports/web
-Remove-Item * -exclude .git
+$path = "exports/web"
 $godot = "C:\Users\gilad\Documents\Programming\Game dev\Godot_v4.3-stable_win64\Godot_v4.3-stable_win64.exe"
+Remove-Item $path/* -exclude .git
 Start-Process -FilePath "$godot" -ArgumentList "--headless --export-debug Web" -NoNewWindow -Wait
-echo here
-# git diff --name-status
-# git add -A
-# git commit -m "new"
-# git push -u origin
-cd ../..
+git -C $path add -A
+git -C $path --no-pager diff HEAD --name-status
+git -C $path commit -m "new"
+git -C $path push -u origin
