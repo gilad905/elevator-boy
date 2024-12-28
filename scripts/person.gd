f6e5dc984d785778df20f8558004227838f590db
Gilad Mayani <gilad905@gmail.com>
2024-12-28 19:35:11 +0100

    fixed face timers after patience reached; moneyIcon; more visuals

--- scripts/person.gd
+++ scripts/person.gd
@@ -11,9 +11,9 @@ var radius = Global.person_radius
 var circle_center = Vector2.ONE * radius
 var zero_angle: float = PI * -0.5
 var full_angle: float = PI * 1.5
-# var angle_third: float = (full_angle - zero_angle) / 3
 var current_angle: float = 0
-const timeout_reached_duration: float = 1
+var face_timers = []
+const patience_ended_tween_duration: float = 1
 
 func _ready() -> void:
 	start_patience_tween()
@@ -25,8 +25,8 @@ func _draw() -> void:
 func start_patience_tween() -> void:
 	patience_tween = create_tween()
 	patience_tween.tween_method(redraw_patience, zero_angle, full_angle, Global.person_patience_sec)
-	add_patience_face_timer(50, "angry_1")
-	add_patience_face_timer(75, "angry_2")
+	add_face_timer(50, "angry_1")
+	add_face_timer(75, "angry_2")
 	await patience_tween.finished
 	is_patience_ended = true
 	patience_ended.emit(self)
@@ -35,10 +35,11 @@ func start_patience_tween() -> void:
 # func add_to_patience(secs: float):
 # 	patience_tween
 
-func add_patience_face_timer(percent: int, state: String) -> void:
+func add_face_timer(percent: int, state: String) -> void:
 	var sec = Global.person_patience_sec * percent / 100.0
-	await get_tree().create_timer(sec).timeout
-	$Face.play(state)
+	var timer = get_tree().create_timer(sec)
+	timer.timeout.connect($Face.play.bind(state))
+	face_timers.append(timer)
 
 func redraw_patience(angle: float) -> void:
 	current_angle = angle
@@ -54,25 +55,29 @@ func move_to(_position):
 
 func set_dest(_dest: int) -> void:
 	dest = _dest
-	$Label.text = str(_dest)
+	$Dest.text = str(_dest)
 
 func show_patience_ended(is_happy: bool):
-	var face_animation = "happy" if is_happy else "angry_3"
-	var mark_animation = "v" if is_happy else "x"
-	var new_scale = $Checkmark.scale.x * 2
-
 	patience_tween.stop()
-	$Face.play(face_animation)
-	$Checkmark.play(mark_animation)
-	$Label.hide()
-	$Checkmark.show()
+	for timer in face_timers:
+		timer.stop()
+	$Dest.hide()
 
+	var duration = patience_ended_tween_duration
 	var tween = create_tween()
-	tween.set_parallel(true)
-	tween.tween_property(self, "modulate:a", 0, timeout_reached_duration)
-	for type in ["x", "y"]:
-		tween.tween_property($Checkmark, "scale:" + type, new_scale, timeout_reached_duration)
-	tween.finished.connect(get_parent().remove_child.bind(self))
-	tween.finished.connect(queue_free)
 
+	if is_happy:
+		var new_scale = $MoneyIcon.scale.x * 2
+		tween.set_parallel(true)
+		for type in ["x", "y"]:
+			tween.tween_property($MoneyIcon, "scale:" + type, new_scale, duration)
+		$Face.play("happy")
+		$MoneyIcon.show()
+
+	tween.tween_property(self, "modulate:a", 0, duration)
+	tween.finished.connect(remove)
 	return tween
+
+func remove() -> void:
+	get_parent().remove_child(self)
+	queue_free()
